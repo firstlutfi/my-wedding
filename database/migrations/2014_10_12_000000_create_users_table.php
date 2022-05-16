c8f417c1cecd293e9c45f5e72b911b037db6baf4
Lutfi Fitroh Hadi <lutfi.fitroh@flip.id>
2022-05-16 12:23:08 +0700

    add table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->string('username')->primary();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
---
+++ database/migrations/2022_05_16_051610_create_guest_list_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guest_list', function (Blueprint $table) {
+            $table->string('invitation_code')->primary();
+            $table->string('guest_name');
+            $table->string('attendance_type');
+            $table->string('rsvp');
+            $table->integer('max_attendance');
+            $table->integer('number_of_attendance');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guest_list');
+    }
+}
