de0d65ab820b729c4539419c2daff51eb523c2cc
Lutfi Fitroh Hadi <lutfi.fitroh@flip.id>
2022-05-16 12:50:31 +0700

    add conditional in boot function AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
+        config('app.APP_ENV') == 'production' ? URL::forceScheme('https') : '';
     }
 }
